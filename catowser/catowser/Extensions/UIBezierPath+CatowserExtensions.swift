080531e1418382028c643826429b362ce4d49e33
Andrey Ermoshin <iermoshin@gmail.com>
2018-02-19 11:40:08 +0300

    moving method to separate file

---
+++ catowser/catowser/Extensions/UIBezierPath+CatowserExtensions.swift
@@ -0,0 +1,50 @@
+//
+//  UIBezierPath+CatowserExtensions.swift
+//  catowser
+//
+//  Created by Andrey Ermoshin on 28/01/2018.
+//  Copyright © 2018 andreiermoshin. All rights reserved.
+//
+
+import UIKit
+
+extension UIBezierPath {
+    enum TopTabsCurveDirection {
+        case right
+        case left
+        case both
+    }
+    
+    class func topTabsCurve(_ width: CGFloat, height: CGFloat, direction: TopTabsCurveDirection) -> UIBezierPath {
+        let x1: CGFloat = 32.84
+        let x2: CGFloat = 5.1
+        let x3: CGFloat = 19.76
+        let x4: CGFloat = 58.27
+        let x5: CGFloat = -12.15
+        
+        // Bezier Drawing
+        let bezierPath = UIBezierPath()
+        bezierPath.move(to: CGPoint(x: width, y: height))
+        switch direction {
+        case .right:
+            bezierPath.addCurve(to: CGPoint(x: width-x1, y: 0), controlPoint1: CGPoint(x: width-x3, y: height), controlPoint2: CGPoint(x: width-x2, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: 0, y: 0), controlPoint1: CGPoint(x: 0, y: 0), controlPoint2: CGPoint(x: 0, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: 0, y: height), controlPoint1: CGPoint(x: 0, y: height), controlPoint2: CGPoint(x: 0, y: height))
+            bezierPath.addCurve(to: CGPoint(x: width, y: height), controlPoint1: CGPoint(x: x5, y: height), controlPoint2: CGPoint(x: width-x5, y: height))
+        case .left:
+            bezierPath.addCurve(to: CGPoint(x: width, y: 0), controlPoint1: CGPoint(x: width, y: 0), controlPoint2: CGPoint(x: width, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: x1, y: 0), controlPoint1: CGPoint(x: width-x4, y: 0), controlPoint2: CGPoint(x: x4, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: 0, y: height), controlPoint1: CGPoint(x: x2, y: 0), controlPoint2: CGPoint(x: x3, y: height))
+            bezierPath.addCurve(to: CGPoint(x: width, y: height), controlPoint1: CGPoint(x: width, y: height), controlPoint2: CGPoint(x: width, y: height))
+        case .both:
+            bezierPath.addCurve(to: CGPoint(x: width-x1, y: 0), controlPoint1: CGPoint(x: width-x3, y: height), controlPoint2: CGPoint(x: width-x2, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: x1, y: 0), controlPoint1: CGPoint(x: width-x4, y: 0), controlPoint2: CGPoint(x: x4, y: 0))
+            bezierPath.addCurve(to: CGPoint(x: 0, y: height), controlPoint1: CGPoint(x: x2, y: 0), controlPoint2: CGPoint(x: x3, y: height))
+            bezierPath.addCurve(to: CGPoint(x: width, y: height), controlPoint1: CGPoint(x: x5, y: height), controlPoint2: CGPoint(x: width-x5, y: height))
+        }
+        bezierPath.close()
+        bezierPath.miterLimit = 4
+        return bezierPath
+    }
+}
+
