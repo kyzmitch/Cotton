c3efe2f9bc54a2379f07d98c9cb34478eccef7d2
Andrey Ermoshin <iermoshin@gmail.com>
2018-02-25 21:56:19 +0300

    Search suggestion client - #1

---
+++ catowser/catowser/Network/SearchSuggestClient.swift
@@ -0,0 +1,79 @@
+//
+//  SearchSuggestClient.swift
+//  catowser
+//
+//  Created by Andrey Ermoshin on 25/02/2018.
+//  Copyright © 2018 andreiermoshin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol WebPageDescription {
+    var webAddress: URL {get set}
+    var title: String {get set}
+    var shortDescription: String {get set}
+    var icon: UIImage {get set}
+}
+
+protocol WebSearchResult {
+    var searchEngineName: String {get set}
+    var numberOfSearchResults: Int {get set}
+    func searchResultDescription(for number: Int) -> WebPageDescription
+}
+
+protocol TextWebSearchResult: WebSearchResult {
+    var searchedText: String {get set}
+}
+
+extension TextWebSearchResult {
+    func searchedKeywords() -> [String] {
+        return searchedText.split(separator: " ").filter {$0.count >= 3}.map {String($0)}
+    }
+}
+
+protocol ImageWebSearchResult: WebSearchResult {
+    var searchedImage: UIImage {get set}
+}
+
+enum SearchResult {
+    case NoResult(Error?)
+    // for example if search queury was "open" then
+    // result array should give something like:
+    // ["openoffice", "openvpn", "opencv",...]
+    // and these strings could be used as titles for
+    // buttons in stack view to allow user
+    // to select from suggested quick variants
+    // without typing whole query
+    case TextSuccess([String])
+    case ImageSuccess([UIImage])
+}
+
+typealias SearchSuggestionsCallback = (_ response: SearchResult) -> Void
+
+protocol SearchClient {
+    var searchEngine: WebSearchEngine {get set}
+    var textSearchCallback: SearchSuggestionsCallback {get set}
+    
+    init(webSearchEngine: WebSearchEngine, textSearchHandler: @escaping SearchSuggestionsCallback)
+    func searchText(searchQuery: String)
+    func searchImage(searchedImage: UIImage)
+}
+
+class SearchSuggestClient: SearchClient {
+    var searchEngine: WebSearchEngine
+    var textSearchCallback: SearchSuggestionsCallback
+    
+    required init(webSearchEngine: WebSearchEngine, textSearchHandler: @escaping SearchSuggestionsCallback) {
+        searchEngine = webSearchEngine
+        textSearchCallback = textSearchHandler
+    }
+    
+    func searchText(searchQuery: String) {
+        
+    }
+    
+    func searchImage(searchedImage: UIImage) {
+        
+    }
+}
