e1ada557b373e362bcc5cb11769bc70106d7d785
Andrei Ermoshin <iermoshin@gmail.com>
2019-01-18 15:47:03 +0300

    Fix search bar behaviour on smartphone

--- catowser/catowser/Search/SmartphoneSearchBarViewController.swift
+++ catowser/catowser/Search/SmartphoneSearchBarViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class SmartphoneSearchBarViewController: BaseViewController, SearchBarControllerInterface {
+class SmartphoneSearchBarViewController: BaseViewController {
 
     let searchBarViewController: SearchBarBaseViewController<SearchSuggestClient<AlamofireHttpClient>>
-    
+
+    private lazy var goBackButton: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = UIColor.white
+        let img = UIImage(named: "goBack")
+        btn.setImage(img, for: .normal)
+
+        return btn
+    }()
+
+    private lazy var backButtonNormalWidth: NSLayoutConstraint = {
+        return goBackButton.widthAnchor.constraint(equalTo: view.heightAnchor)
+    }()
+
+    private lazy var backButtonZeroWidth: NSLayoutConstraint = {
+        return goBackButton.widthAnchor.constraint(equalToConstant: 0.0)
+    }()
+
     init(_ searchSuggestionsClient: SearchSuggestClient<AlamofireHttpClient>, _ searchBarDelegate: UISearchBarDelegate) {
         searchBarViewController = SearchBarBaseViewController(searchSuggestionsClient, searchBarDelegate)
         super.init(nibName: nil, bundle: nil)
@@ -21,37 +38,41 @@ class SmartphoneSearchBarViewController: BaseViewController, SearchBarController
         fatalError("init(coder:) has not been implemented")
     }
     
-    lazy var goBackButton: UIButton = {
-        let btn = UIButton()
-        btn.backgroundColor = UIColor.white
-        let img = UIImage(named: "goBack")
-        btn.setImage(img, for: .normal)
-        
-        return btn
-    }()
-    
-    override func loadView() {
-        view = UIView()
-        
-        view.addSubview(goBackButton)
-        add(asChildViewController: searchBarViewController, to:view)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(goBackButton)
+        add(asChildViewController: searchBarViewController, to:view)
         
-        goBackButton.snp.makeConstraints { (maker) in
-            maker.leading.equalTo(0)
-            maker.top.equalTo(0)
-            maker.bottom.equalTo(0)
-            maker.width.equalTo(view.snp.height)
-        }
-        
-        searchBarViewController.view.snp.makeConstraints { (maker) in
-            maker.leading.equalTo(goBackButton.snp.trailing)
-            maker.top.equalTo(0)
-            maker.bottom.equalTo(0)
-            maker.trailing.equalTo(0)
+        goBackButton.translatesAutoresizingMaskIntoConstraints = false
+        searchBarViewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        goBackButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        goBackButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        goBackButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        backButtonNormalWidth.isActive = true
+
+        searchBarViewController.view.leadingAnchor.constraint(equalTo: goBackButton.trailingAnchor, constant: 0).isActive = true
+        searchBarViewController.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        searchBarViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        searchBarViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+    }
+}
+
+extension SmartphoneSearchBarViewController: SearchBarControllerInterface {
+    func stateChanged(to state: SearchBarState) {
+        switch state {
+        case .readyForInput:
+            backButtonNormalWidth.isActive = false
+            backButtonZeroWidth.isActive = true
+            searchBarViewController.searchBarView.setShowsCancelButton(true, animated: true)
+        case .clear:
+            // You always want to first deactivate the old one and then activate
+            // the new one, otherwise for that moment when you activate the new
+            // one it would conflict with the old one and cause warnings in console.
+            backButtonZeroWidth.isActive = false
+            backButtonNormalWidth.isActive = true
+            searchBarViewController.searchBarView.setShowsCancelButton(false, animated: false)
         }
     }
 
