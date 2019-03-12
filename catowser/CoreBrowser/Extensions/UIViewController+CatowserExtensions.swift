da1fa923ca1b95bfadf5a03db9570b92459e80ac
Andrei Ermoshin <iermoshin@gmail.com>
2019-03-12 15:35:24 +0300

    moving files to framework

---
+++ catowser/CoreBrowser/Extensions/UIViewController+CatowserExtensions.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController+CatowserExtensions.swift
+//  catowser
+//
+//  Created by Andrei Ermoshin on 21/02/2019.
+//  Copyright © 2019 andreiermoshin. All rights reserved.
+//
+
+import UIKit
+
+public extension UIViewController {
+    func removeFromChild() {
+        willMove(toParent: nil)
+        removeFromParent()
+        // remove view and constraints
+        view.removeFromSuperview()
+    }
+
+    func add(asChildViewController viewController: UIViewController, to containerView: UIView) {
+        // from docs `willMove` will be called automatically inside `addChild`
+        addChild(viewController)
+        // No need add constraints, they need to added specifically for each view
+        // because view size can vary
+        containerView.addSubview(viewController.view)
+        viewController.didMove(toParent: self)
+    }
+}
