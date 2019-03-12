01829eb1b22c4586993c49e81dff1ebe3d1d8d6e
Andrei Ermoshin <iermoshin@gmail.com>
2019-03-12 15:35:24 +0300

    moving files to framework

--- catowser/catowser/BaseViewController.swift
+++ catowser/catowser/BaseViewController.swift
@@ -9,16 +9,4 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-
-}
-
-extension UIViewController {
-    func add(asChildViewController viewController: UIViewController, to containerView: UIView) {
-        // from docs `willMove` will be called automatically inside `addChild`
-        addChild(viewController)
-        // No need add constraints, they need to added specifically for each view
-        // because view size can vary
-        containerView.addSubview(viewController.view)
-        viewController.didMove(toParent: self)
-    }
 }
