901480094069b1f95dcd66c89982af727ec955d3
Andrei Ermoshin <iermoshin@gmail.com>
2019-03-13 15:25:36 +0300

    first draft js file

---
+++ catowser/CoreBrowser/JavaScripts/ajaxHandler.js
@@ -0,0 +1,15 @@
+// Every time an Ajax call is being invoked the listener will recognize it and  will call the native app with the request details
+// https://stackoverflow.com/questions/28766676/how-can-i-monitor-requests-on-wkwebview
+
+$( document ).ajaxSend(function( event, request, settings )  {
+    callNativeApp (settings.data);
+});
+
+function callNativeApp (data) {
+    try {
+        webkit.messageHandlers.callbackHandler.postMessage(data);
+    }
+    catch(err) {
+        console.log('The native context does not exist yet');
+    }
+}
