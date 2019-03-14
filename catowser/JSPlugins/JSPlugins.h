5bcd344a7119eb5a12b00e753eafb813afa04896
Andrei Ermoshin <iermoshin@gmail.com>
2019-03-14 10:39:50 +0300

    create separate framework for js files and logic

---
+++ catowser/JSPlugins/JSPlugins.h
@@ -0,0 +1,19 @@
+//
+//  JSPlugins.h
+//  JSPlugins
+//
+//  Created by Andrei Ermoshin on 14/03/2019.
+//  Copyright © 2019 andreiermoshin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSPlugins.
+FOUNDATION_EXPORT double JSPluginsVersionNumber;
+
+//! Project version string for JSPlugins.
+FOUNDATION_EXPORT const unsigned char JSPluginsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSPlugins/PublicHeader.h>
+
+
