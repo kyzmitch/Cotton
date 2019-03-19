28e2af5d44a718a3e68d10b160a77d3bc4d96e3b
Andrei Ermoshin <iermoshin@gmail.com>
2019-03-19 15:05:59 +0300

    script now works

--- catowser/JSPlugins/Scripts/ig.js
+++ catowser/JSPlugins/Scripts/ig.js
@@ -1,24 +1,46 @@
 
 "use strict";
 
-var ig_video_url = get_ig_meta('og:video');
-// var ig_image_url = get_ig_meta('og:image');
-
-if(ig_video_url){
-	try {
-        webkit.messageHandlers.igHandler.postMessage("url":ig_video_url);
+if(window.onload) {
+    try {
+        webkit.messageHandlers.igHandler.postMessage({"log":"Window already has onload handler"});
     } catch(err) {
-        webkit.messageHandlers.igHandler.postMessage("log":"The native context does not exist yet");
+        console.log('Window already has onload handler');
     }
+	var currenload = window.onload;
+	var newonload = function(evt) {
+        searchIgVideoLink();
+		currenload(evt);
+	};
+	window.onload = newonload;
+} else {
+	window.onload = searchIgVideoLink;
 }
-else {
-    webkit.messageHandlers.igHandler.postMessage("log":"No any video url was found");
+
+function searchIgVideoLink() {
+	var ig_video_url = get_ig_meta('og:video');
+
+	if(ig_video_url){
+		console.log('found url' + ig_video_url);
+		try {
+			webkit.messageHandlers.igHandler.postMessage({"url": ig_video_url});
+    	} catch(err) {
+			console.log('The native context does not exist yet');
+    	}
+	}
+	else {
+		try {
+			webkit.messageHandlers.igHandler.postMessage({"log":"No any video url was found"});
+		} catch(err) {
+			console.log('No any video url was found');
+		}
+	}
 }
 
 function get_ig_meta(key) {
 	var metas = document.getElementsByTagName('meta');
 
-	for (i=0; i<metas.length; i++){
+	for (var i=0; i<metas.length; i++){
 		if (metas[i].getAttribute("property") == key){
 			return metas[i].getAttribute("content");
 		}
