de3004772714b7e1b28c64e0943febc108e43eaf
Andrei Ermoshin <iermoshin@gmail.com>
2019-04-09 14:21:22 +0300

    remove buggy code

--- catowser/catowser/Downloads/LinksBadgeView.swift
+++ catowser/catowser/Downloads/LinksBadgeView.swift
@@ -12,19 +12,6 @@ import CoreBrowser
 final class LinksBadgeView: UICollectionViewCell, ReusableItem {
     @IBOutlet weak var tagTypeLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-        contentView.bounds = bounds
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        tagTypeLabel.layer.cornerRadius = tagTypeLabel.bounds.size.height / 2
-        tagTypeLabel.clipsToBounds = true
-    }
-
     override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
         let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
         var newFrame = layoutAttributes.frame
