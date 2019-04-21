274e89a7d2a2b8d694ba987c4f9b655aa86ffa3a
Andrei Ermoshin <iermoshin@gmail.com>
2019-04-21 17:03:11 +0300

    changes not added to commit

---
+++ catowser/JSPlugins/Scripts/js/t4.js
@@ -0,0 +1,39 @@
+"use strict";
+
+XMLHttpRequest.prototype.cottonRealSend = XMLHttpRequest.prototype.send;
+XMLHttpRequest.prototype.send = function(body) {
+	this.cottonRealSend(body);
+	this.addEventListener('readystatechange', function() {
+		if (this.readyState !== 4 /* DONE */ || this.status !== 200) {
+			return;
+		}
+		
+		// this.responseType is empty for some reason, so it's not possible to parse 
+		// for json specifically
+		cottonLog('HttpResponse 200 OK:' + this.responseURL);
+		cottonHandleT4HttpResponseText(this.responseText);
+	});
+};
+
+function cottonHandleT4HttpResponseText(text) {
+    let json = JSON.parse(text);
+    if(typeof json === 'undefined'){
+        return;
+    }
+    if(json.length == 0){
+        return;
+    }
+
+    for (let i=0; i<json.length; i++) {
+        console.log('video node[' + i + '] with url: ' + nodes[i]['video_url']);
+    }
+
+    function sendVideoNodesToNativeApp(nodes) {
+        
+        try {
+            webkit.messageHandlers.igHandler.postMessage({"videoNodes": JSON.stringify(nodes)});
+        } catch(err) {
+            console.log('the native context does not exist yet');
+        }
+    }
+}
