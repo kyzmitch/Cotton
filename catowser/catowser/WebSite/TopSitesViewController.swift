444d49c1bc7e3e1aab3cc53252a6c286fc41aca3
Andrei Ermoshin <iermoshin@gmail.com>
2019-04-22 12:07:41 +0300

    small fixes

---
+++ catowser/catowser/WebSite/TopSitesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TopSitesViewController.swift
+//  catowser
+//
+//  Created by Andrei Ermoshin on 22/04/2019.
+//  Copyright © 2019 andreiermoshin. All rights reserved.
+//
+
+import UIKit
+
+class TopSitesViewController: UIViewController {
+
+}
