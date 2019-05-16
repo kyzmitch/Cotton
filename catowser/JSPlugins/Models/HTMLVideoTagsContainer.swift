4a72a239ffc2ccf26c9f83828bdac20842fe4051
Andrey Ermoshin <iermoshin@gmail.com>
2019-05-16 22:00:29 +0300

    start html video tags parsing

--- catowser/JSPlugins/Models/HTMLVideoTagsContainer.swift
+++ catowser/JSPlugins/Models/HTMLVideoTagsContainer.swift
@@ -11,12 +11,36 @@ import Foundation
 public struct HTMLVideoTagsContainer {
     public let videoURLs: [URL]
     init(html: String) throws {
-        throw CottonError.noTags
+        guard html.count > 0 else {
+            throw CottonError.emptyHtml
+        }
+        
+        let tagsStartIndexes = html.indices(of: "<video")
+        let tagsEndIndexes = html.indices(of: "</video>")
+        let count = tagsStartIndexes.count
+        guard count != 0, count == tagsEndIndexes.count else {
+            assertionFailure("Unexpected tags count start: \(tagsStartIndexes.count) end: \(tagsEndIndexes.count)")
+            throw CottonError.noVideoTags
+        }
+        
+        let sourcURLParam = "src=\""
+        for i in (0..<count) {
+            let sIndex = tagsStartIndexes[i]
+            let eIndex = tagsEndIndexes[i]
+            let videoTagString = html[sIndex..<eIndex]
+            
+            guard let sourceURLParamRange = videoTagString.range(of: sourcURLParam) else {
+                continue
+            }
+            
+            let sourceSubstring = html[sourceURLParamRange.lowerBound..<eIndex]
+        }
     }
 }
 
 extension HTMLVideoTagsContainer {
     enum CottonError: Error {
-        case noTags
+        case emptyHtml
+        case noVideoTags
     }
 }
