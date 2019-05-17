e14c1c79f608f8781a0fc308ddc8e2374b103a24
Andrey Ermoshin <iermoshin@gmail.com>
2019-05-17 07:43:47 +0300

    start impl video tags parsing

---
+++ catowser/JSPlugins/Models/HTMLVideoTag.swift
@@ -0,0 +1,14 @@
+//
+//  HTMLVideoTag.swift
+//  JSPlugins
+//
+//  Created by Andrey Ermoshin on 17/05/2019.
+//  Copyright © 2019 andreiermoshin. All rights reserved.
+//
+
+import Foundation
+
+public struct HTMLVideoTag {
+    let src: URL
+    let poster: URL
+}
