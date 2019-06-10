fd5f75b9ea6212e1d29dd0abf853ee196a46091c
Andrei Ermoshin <iermoshin@gmail.com>
2019-06-10 11:20:52 +0300

    refactor and fix for iphone tabs previews

--- catowser/catowser/AppDelegate.swift
+++ catowser/catowser/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let themeType: ThemeType = .default
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // This is to fix favicon.ico for Instagram site, calling it once
+        DataRequest.addAcceptableImageContentTypes(["image/vnd.microsoft.icon"])
+        
         window = UIWindow(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
         let rootViewController = MasterBrowserViewController()
         window?.rootViewController = rootViewController
