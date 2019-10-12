f3a30f7c0e04e816b0af1c466fb444db13b0b647
Andrei Ermoshin <iermoshin@gmail.com>
2019-10-12 16:24:36 +0300

    In process

--- catowser/HttpKit/HttpClient.swift
+++ catowser/HttpKit/HttpClient.swift
@@ -7,7 +7,41 @@
 //
 
 import Foundation
+import Alamofire
+import ReactiveSwift
 
 /// Main namespace for Kit
 public enum HttpKit {}
 
+extension HttpKit {
+    public class Client {
+        private let server: ServerDescription
+        
+        private let connectivityManager: NetworkReachabilityManager?
+        
+        private let httpTimeout: TimeInterval
+        
+        private lazy var hostListener: Alamofire.NetworkReachabilityManager.Listener = { [weak self] status in
+            guard let self = self else {
+                return
+            }
+            // TODO: set connectionStateStream.value
+        }
+        
+        public init(server: ServerDescription, httpTimeout: TimeInterval = 60) {
+            self.server = server
+            self.httpTimeout = httpTimeout
+            
+            if let manager = NetworkReachabilityManager(host: server.hostString) {
+                connectivityManager = manager
+            } else if let manager = NetworkReachabilityManager() {
+                connectivityManager = manager
+            } else {
+                connectivityManager = nil
+                assertionFailure("No connectivity manager for: \(server.hostString)")
+            }
+        }
+        
+        
+    }
+}
