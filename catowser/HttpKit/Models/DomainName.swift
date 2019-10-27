5a1f7fdd96eb56b84c43e3570724bc0882576dfb
Andrei Ermoshin <iermoshin@gmail.com>
2019-10-27 20:01:47 +0300

    Add usage of standard character set for url and add more checks to DomainName

--- catowser/HttpKit/Models/DomainName.swift
+++ catowser/HttpKit/Models/DomainName.swift
@@ -14,20 +14,42 @@ extension HttpKit {
         
         /// https://developers.google.com/speed/public-dns/docs/doh/json
         public init(_ name: String) throws {
+            // Invalid names like .example.com, example..com or empty string get 400 Bad Request.
+            
+            guard !name.isEmpty else {
+                throw DomainNameError.emptyString
+            }
+            
+            if let first = name.first, first == "." {
+                throw DomainNameError.dotAtBeginning
+            }
+            if name.contains("..") {
+                throw DomainNameError.doubleDots
+            }
             // name: The length (after replacing backslash escapes) must be
             // between 1 and 253 (ignoring an optional trailing dot if present).
             
             let nLenght = name.count
             guard (1...253).contains(nLenght) else {
-                throw DomainNameError.wrongLength
+                throw DomainNameError.wrongLength(nLenght)
             }
             
             // All labels (parts of the name betweendots) must be 1 to 63 bytes long.
-            
-            // Invalid names like .example.com, example..com or empty string get 400 Bad Request.
+            let parts = name.split(separator: ".")
+            for part in parts {
+                guard let partData = part.data(using: .ascii) else {
+                    continue
+                }
+                guard (1...63).contains(partData.count) else {
+                    throw DomainNameError.wrongPartSize(partData.count)
+                }
+            }
             
             // Non-ASCII characters should be punycoded (xn--qxam, not ελ).
             
+            for character in name {
+                if CharacterSet.urlHostAllowed.co
+            }
             self.string = name
         }
     }
@@ -35,9 +57,12 @@ extension HttpKit {
 
 extension HttpKit {
     public enum DomainNameError: Error {
-        case wrongLength
+        case wrongLength(Int)
         case wrongLabels
         case invalidName
-        
+        case emptyString
+        case dotAtBeginning
+        case doubleDots
+        case wrongPartSize(Int)
     }
 }
