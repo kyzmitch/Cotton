d402e8e19bb8ddcb4db432e6e11d72a081a742fd
Andrei Ermoshin <iermoshin@gmail.com>
2020-04-20 20:32:53 +0300

    Partly implement OpenSearch parsing format to use many different search engines

--- catowser/HttpKit/SearchEngines/SearchEngine+OpenSearchParser.swift
+++ catowser/HttpKit/SearchEngines/SearchEngine+OpenSearchParser.swift
@@ -8,21 +8,85 @@
 
 import Foundation
 import SWXMLHash
+import Alamofire // for HTTPMethod type
+
+/**
+ https://developer.mozilla.org/en-US/docs/Web/OpenSearch
+ 
+ 
+ */
 
 public enum OpenSearchError: LocalizedError {
-    case invalidFile
+    case noAnyURLXml
+    case noTemplateParameter
+    case templateIsNotURL
+    case notValidURL
+    case htmlTemplateUrlNotFound
+}
+
+enum ImageEncoding: String {
+    case xIcon = "image/x-icon"
 }
 
 public extension HttpKit.SearchEngine {
-    init(_ file: String, engineID: String) throws {
-        guard let data = try? Data(contentsOf: URL(fileURLWithPath: file)) else {
-            throw OpenSearchError.invalidFile
-        }
-
-        _ = SWXMLHash.config { (options) in
-                options.detectParsingErrors = true
-        }.parse(data)
+    init(xml element: XMLElement, shortName: String, imageData: Data? = nil) throws {
+        self.shortName = shortName
+        self.imageData = imageData
         
-        self = HttpKit.SearchEngine.googleSearchEngine()
+        let httpMethod: HTTPMethod
+        if let httpMethodString = element.attribute(by: "method")?.text {
+            httpMethod = HTTPMethod(rawValue: httpMethodString) ?? .get
+        } else {
+            httpMethod = .get
+        }
+        self.httpMethod = httpMethod
+        
+        let optionalTemplateString = element.attribute(by: "template")?.text
+        guard let templateString = optionalTemplateString else {
+            throw OpenSearchError.noTemplateParameter
+        }
+        guard let url = URL(string: templateString) else {
+            throw OpenSearchError.templateIsNotURL
+        }
+        let optionalComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        guard let components = optionalComponents else {
+            throw OpenSearchError.notValidURL
+        }
+        self.components = components
+        
+        // TODO: parse Params from xml as well
+        self.queryItems = components.queryItems ?? []
+    }
+    
+    private static func parseOpenSearchURLParams() -> [URLQueryItem]? {
+        // TODO: implement
+        return nil
     }
 }
+
+extension Data {
+    static func parseOpenSearchImage(_ imageXmlElement: XMLIndexer) -> Data? {
+        let imageData: Data?
+        
+        if let encodedImageString = imageXmlElement.element?.text {
+            let imgWidthStr = imageXmlElement.element?.attribute(by: "width")?.text ?? "16"
+            let imgHeightStr = imageXmlElement.element?.attribute(by: "height")?.text ?? "16"
+            _ = Int(imgWidthStr, radix: 10) ?? 16
+            _ = Int(imgHeightStr, radix: 10) ?? 16
+            let imgEncodingTypeStr = imageXmlElement.element?.attribute(by: "type")?.text
+            if let encodingTypeStr = imgEncodingTypeStr,
+                let _ = ImageEncoding(rawValue: encodingTypeStr) {
+                // TODO: add handling for x-icon and for other formats
+                imageData = Data(base64Encoded: encodedImageString)
+            } else {
+                // probably base64
+                imageData = Data(base64Encoded: encodedImageString)
+            }
+        } else {
+            imageData = nil
+        }
+        
+        return imageData
+    }
+}
+
--- catowser/HttpKit/SearchEngines/SearchEngine.swift
+++ catowser/HttpKit/SearchEngines/SearchEngine.swift
@@ -7,17 +7,28 @@
 //
 
 import Foundation
+import Alamofire // only for Http method type
 
 extension HttpKit {
+    /// Provides URLs for HTML content type to use it in web views
     public struct SearchEngine {
         /// Name e.g. "DuckDuckGo"
-        private let shortName: String
+        let shortName: String
         /// Valid components for final URL
-        private let components: URLComponents
+        let components: URLComponents
         /// Original query items
-        private let queryItems: [URLQueryItem]
+        let queryItems: [URLQueryItem]
+        /// Data for site image/icon
+        let imageData: Data?
+        /// So far not used parameter,  but it present in OpenSearch format
+        let httpMethod: HTTPMethod
         
-        init(shortName: String, domainName: String, path: String, queryItems: [URLQueryItem]) {
+        init(shortName: String,
+             domainName: String,
+             path: String,
+             queryItems: [URLQueryItem],
+             imageData: Data? = nil,
+             httpMethod: HTTPMethod = .get) {
             self.shortName = shortName
             
             var components = URLComponents()
@@ -26,6 +37,8 @@ extension HttpKit {
             components.path = path
             self.components = components
             self.queryItems = queryItems
+            self.imageData = imageData
+            self.httpMethod = httpMethod
         }
 
         /// Returns the search URL for the given query.
