abf791fab8a6cac796bfb717ba607caa8c566ed6
Andrei Ermoshin <iermoshin@gmail.com>
2020-05-20 22:42:50 +0300

    Implement monitor for application features changes

--- catowser/catowser/Utils/Features/FeatureManager.swift
+++ catowser/catowser/Utils/Features/FeatureManager.swift
@@ -7,6 +7,10 @@
 //
 
 import Foundation
+#if canImport(Combine)
+import Combine
+#endif
+import ReactiveSwift
 
 final class FeatureManager {
     private static let shared: FeatureManager = .init()
@@ -28,7 +32,27 @@ final class FeatureManager {
         source.setValue(of: feature, value: value)
     }
     
-    // TODO: implement feature state observer (e.g. needed to reload web view)
+    @available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+    typealias AppFeaturePublisher<F: Feature> = AnyPublisher<ApplicationFeature<F>, Never>
+    
+    @available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+    static func featureChangesPublisher<F>(for feature: ApplicationFeature<F>) -> AppFeaturePublisher<F> {
+        guard let source = source(for: feature) as? ObservableFeatureSource else {
+            let empty = Empty<ApplicationFeature<F>, Never>(completeImmediately: false)
+            return empty.eraseToAnyPublisher()
+        }
+        return source.futureFeatureChanges
+            .compactMap { $0 == feature ? feature : nil }
+            .eraseToAnyPublisher()
+    }
+    
+    static func rxFeatureChanges<F>(for feature: ApplicationFeature<F>) -> Signal<ApplicationFeature<F>, Never> {
+        guard let source = source(for: feature) as? ObservableFeatureSource else {
+            return .empty
+        }
+        return source.rxFutureFeatureChanges
+            .filterMap { $0 == feature ? feature : nil }
+    }
     
     private static func source<F>(for feature: ApplicationFeature<F>) -> FeatureSource? {
         return shared.sources.first(where: { type(of: $0) == F.source })
