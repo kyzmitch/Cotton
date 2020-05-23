493fceb7988cb44ba194e0c18ff0b45f212e3542
Andrei Ermoshin <iermoshin@gmail.com>
2020-05-23 19:53:33 +0300

    Fix double adding of Alamofire by using common dependency framework, the only bad thing is that HttpKit will have AlamofireImage as a dependency as well, but it won't be used

---
+++ catowser/SPMCommon/SPMCommon.h
@@ -0,0 +1,19 @@
+//
+//  SPMCommon.h
+//  SPMCommon
+//
+//  Created by Andrei Ermoshin on 5/23/20.
+//  Copyright © 2020 andreiermoshin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMCommon.
+FOUNDATION_EXPORT double SPMCommonVersionNumber;
+
+//! Project version string for SPMCommon.
+FOUNDATION_EXPORT const unsigned char SPMCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMCommon/PublicHeader.h>
+
+
