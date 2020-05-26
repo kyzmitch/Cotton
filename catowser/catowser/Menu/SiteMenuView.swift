c07c65df3abfcf6d66cc86958a6d3f035a30b77b
Andrei Ermoshin <iermoshin@gmail.com>
2020-05-26 10:16:02 +0300

    Rename schemes and change target names from catowser

---
+++ catowser/catowser/Menu/SiteMenuView.swift
@@ -0,0 +1,28 @@
+//
+//  SiteMenuView.swift
+//  catowser
+//
+//  Created by Andrei Ermoshin on 5/25/20.
+//  Copyright © 2020 andreiermoshin. All rights reserved.
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+
+@available(iOS 13.0.0, *)
+struct SiteMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+// swiftlint:disable type_name
+@available(iOS 13.0.0, *)
+struct SiteMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SiteMenuView()
+    }
+}
+#endif
