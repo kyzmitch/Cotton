40ad366fd62bb5518a952ee29229c1ffae253679
Andrei Ermoshin <iermoshin@gmail.com>
2020-05-28 20:39:07 +0300

    Move protocols to different file

--- catowser/CoreBrowser/Tabs/TabsListManager.swift
+++ catowser/CoreBrowser/Tabs/TabsListManager.swift
@@ -1,5 +1,5 @@
 //
-//  TabsSubject.swift
+//  TabsListManager.swift
 //  catowser
 //
 //  Created by Andrei Ermoshin on 28/01/2019.
@@ -8,106 +8,6 @@
 
 import Foundation
 import ReactiveSwift
-
-/// Describes how new tab is added to the list.
-/// Uses `Int` as raw value to be able to store it in settings.
-public enum AddedTabPosition: Int {
-    case listEnd = 0
-    case afterSelected = 1
-}
-
-public enum TabAddSpeed {
-    case immediately
-    case after(DispatchTimeInterval)
-}
-
-/// MARK: Tabs observer protocol.
-public protocol TabsObserver {
-    /// To be able to search specific observer.
-    var name: String { get }
-    /// Updates observer with tabs count.
-    ///
-    /// - Parameter tabsCount: New number of tabs.
-    func update(with tabsCount: Int)
-    /// Prodive necessary data to render UI on tablets
-    ///
-    /// - Parameter tabs: Tabs from cache at application start.
-    func initializeObserver(with tabs: [Tab])
-    /// Tells other observers about new tab.
-    /// We can pause drawing new tab on view layer
-    /// to be able firstly determine type of initial tab state.
-    ///
-    /// - parameters:
-    ///     - tab: new tab
-    ///     - index: where to add new object
-    func tabDidAdd(_ tab: Tab, at index: Int)
-    /// Tells observer that index has changed.
-    ///
-    /// - parameters:
-    ///     - index: new selected index.
-    ///     - content: Tab content, e.g. can be site. Need to pass it to allow browser to change content in web view.
-    func didSelect(index: Int, content: Tab.ContentType)
-    /// Notifies about tab content type changes or `site` changes
-    func tabDidReplace(_ tab: Tab, at index: Int)
-
-    /// No need to add delegate methods for tab close case.
-    /// because anyway view must be removed right away.
-}
-
-// Marks optional functions for protocol
-public extension TabsObserver {
-    var name: String {
-        return String(describing: self)
-    }
-
-    func didSelect(index: Int, content: Tab.ContentType) {
-        // Only landscape/regular tabs list view use that
-    }
-
-    func tabDidAdd(_ tab: Tab, at index: Int) {
-        // e.g. Counter view doesn't need to handle that
-        // as it uses another delegate method with `tabsCount`
-    }
-
-    /* optional */ func tabDidReplace(_ tab: Tab, at index: Int) {}
-
-    /* optional */ func update(with tabsCount: Int) {}
-
-    /* optional */ func initializeObserver(with tabs: [Tab]) {}
-}
-
-public protocol TabsPositioning {
-    var defaultPosition: AddedTabPosition { get }
-    var contentState: Tab.ContentType { get }
-    var addSpeed: TabAddSpeed { get }
-}
-
-public protocol TabsSubject {
-    init(storage: TabsStorage, positioning: TabsPositioning)
-    /// Add tabs observer.
-    func attach(_ observer: TabsObserver)
-    /// Removes tabs observer.
-    func detach(_ observer: TabsObserver)
-    /// Adds tab to memory and storage. Tab can be blank or it can contain URL address.
-    /// Tab will be added no matter what happen, so, function doesn't return any result.
-    ///
-    /// - Parameter tab: A tab.
-    func add(tab: Tab)
-    /// Closes tab.
-    func close(tab: Tab)
-    /// Closes all tabs.
-    func closeAll()
-    /// Remembers selected tab index. Can fail silently if `tab` will not be found in a list.
-    func select(tab: Tab)
-    /// Convinient method to select a tab when you don't have `Tab` object at place.
-    ///
-    /// - Returns: selected tab or nothing if it was not found.
-    func selectTab(at indexPath: IndexPath) -> Tab?
-    /// Replaces currently active tab by combining two operations
-    func replaceSelected(tabContent: Tab.ContentType) throws
-    /// Fetches latest tabs.
-    func fetch() -> [Tab]
-}
 
 public final class TabsListManager {
     // Can't really implement this in Singletone pattern due to
