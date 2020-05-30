3b7ce8623a64d018595e3bf33cff205ad80f8497
Andrei Ermoshin <iermoshin@gmail.com>
2020-05-30 15:57:17 +0300

    Add new SwiftUI view for setting

--- catowser/CoreBrowser/Tabs/TabsSubjectObserver.swift
+++ catowser/CoreBrowser/Tabs/TabsSubjectObserver.swift
@@ -11,7 +11,7 @@ import ReactiveSwift
 
 /// Describes how new tab is added to the list.
 /// Uses `Int` as raw value to be able to store it in settings.
-public enum AddedTabPosition: Int {
+public enum AddedTabPosition: Int, CaseIterable {
     case listEnd = 0
     case afterSelected = 1
 }
