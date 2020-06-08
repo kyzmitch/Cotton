46606117018f381c2573139b5068fce48490c373
Andrei Ermoshin <iermoshin@gmail.com>
2020-06-08 22:02:17 +0300

    Add CssParser overkill to parse tiny css to get preview url

---
+++ catowser/CssParser/CssParser.h
@@ -0,0 +1,19 @@
+//
+//  CssParser.h
+//  CssParser
+//
+//  Created by Andrei Ermoshin on 6/8/20.
+//  Copyright © 2020 andreiermoshin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CssParser.
+FOUNDATION_EXPORT double CssParserVersionNumber;
+
+//! Project version string for CssParser.
+FOUNDATION_EXPORT const unsigned char CssParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CssParser/PublicHeader.h>
+
+
