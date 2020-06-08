01060e3e5c916672d45b4fe3f8832405eca3926b
Andrei Ermoshin <iermoshin@gmail.com>
2020-06-08 22:28:52 +0300

    Created unit test for JSPlugins, but it fails without error silently

---
+++ catowser/JSPluginsTests/CSSBackgroundImageTests.swift
@@ -0,0 +1,24 @@
+//
+//  JSPluginsTests.swift
+//  JSPluginsTests
+//
+//  Created by Andrei Ermoshin on 6/8/20.
+//  Copyright © 2020 andreiermoshin. All rights reserved.
+//
+
+import XCTest
+@testable import JSPlugins
+
+class CSSBackgroundImageTests: XCTestCase {
+
+    /// Tests css from style of ytp-cued-thumbnail-overlay-image class div from youtube
+    /// it should give a valid URL
+    func testYoutubeVideoPreviewURL() throws {
+        let input1 = "background-image: url(&quot;https://example.com/picture.png&quot;);"
+        let possibleBackgroundImage1 = CSSBackgroundImage(cssString: input1)
+        XCTAssertNotNil(possibleBackgroundImage1)
+        // swiftlint:disable:next force_unwrapping
+        let backgroundImage1 = possibleBackgroundImage1!
+        XCTAssertFalse(backgroundImage1.urls.isEmpty)
+    }
+}
