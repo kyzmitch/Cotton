063297fb0eb289a688a0568bc29e301fd44072af
Andrei Ermoshin <iermoshin@gmail.com>
2020-10-11 08:50:10 +0300

    Prepare tabs resource & store

---
+++ catowser/catowser/DB/TabsStore.swift
@@ -0,0 +1,32 @@
+//
+//  TabsStore.swift
+//  catowser
+//
+//  Created by Andrei Ermoshin on 9/28/20.
+//  Copyright © 2020 andreiermoshin. All rights reserved.
+//
+
+import CoreData
+import CoreBrowser
+
+final class TabsStore {
+    private let managedContext: NSManagedObjectContext
+    
+    init(_ managedContext: NSManagedObjectContext) {
+        self.managedContext = managedContext
+    }
+    
+    func insert(tab: Tab) throws {
+        managedContext.performAndWait {
+            
+        }
+    }
+    
+    func remove(tab: Tab) throws {
+        
+    }
+    
+    func fetchAllTabs() throws -> [Tab] {
+        return []
+    }
+}
--- catowser/catowser/Environment/TabsEnvironment.swift
+++ catowser/catowser/Environment/TabsEnvironment.swift
@@ -12,10 +12,15 @@ final class TabsEnvironment {
     static let shared: TabsEnvironment = .init()
     
     let cachedTabsManager: TabsListManager
+    let cottonDb: Database
     
     private init() {
         cachedTabsManager = .init(storage: TabsCacheProvider.shared,
                                   positioning: DefaultTabProvider.shared)
+        guard let database = Database(name: "CottonDbModel") else {
+            fatalError("Failed to initialize CoreData database")
+        }
+        cottonDb = database
     }
 }
 
