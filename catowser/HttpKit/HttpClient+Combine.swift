120014c13e063a1440902f1b6debdf3cb5c30c42
Andrei Ermoshin <iermoshin@gmail.com>
2022-02-18 09:25:26 +0300

    Fix combine implementation

--- catowser/HttpKit/HttpClient+Combine.swift
+++ catowser/HttpKit/HttpClient+Combine.swift
@@ -12,13 +12,15 @@ import Combine
 #endif
 
 extension HttpKit.Client {
-    public typealias ResponseFuture<T> = Publishers.HandleEvents<Deferred<Future<T, HttpKit.HttpError>>>
+    public typealias ResponseFuture<T> = Deferred<Publishers.HandleEvents<Future<T, HttpKit.HttpError>>>
     
     public func cMakeRequest<T, B: HTTPRxAdapter, RX>(for endpoint: HttpKit.Endpoint<T, Server>,
                                                       withAccessToken accessToken: String?,
                                                       transport adapter: B,
                                                       _ subscriber: RxSubscriber<T, Server, RX>) -> ResponseFuture<T>
     where B.Response == T, B.Server == Server, B.ObserverWrapper == RX {
+        // Can't use Future without Deferred because
+        // a Future will begin executing immediately when you create it.
         return Combine.Deferred {
             let subject: Future<T, HttpKit.HttpError> = .init { [weak self] (promise) in
                 guard let self = self else {
@@ -31,12 +33,16 @@ extension HttpKit.Client {
                 // TODO: use makeRequest instead
                 self.makeRxRequest(for: endpoint, withAccessToken: accessToken, transport: adapter)
             }
-            return subject
-        }.handleEvents(receiveCompletion: { [weak subscriber, weak adapter] _ in
-            guard let adapter = adapter else {
-                return
-            }
-            subscriber?.remove(adapter.handlerType)
-        })
+            return subject.handleEvents(receiveCompletion: { [weak subscriber] _ in
+                // Must capture `adapter` by strong reference comparing to
+                // similar implementation for Rx, because the `adapter` is getting deallocated
+                // for Combine implementation for some still unknown reason.
+                // Actually we only store handlerType reference types in the ClientSubscriber sets
+                // so, actually the first question is why Rx implementation was working
+                // because I would expect that adapter is nil and you can't transfer execution back
+                // to the handler
+                subscriber?.remove(adapter.handlerType)
+            })
+        }
     }
 }
