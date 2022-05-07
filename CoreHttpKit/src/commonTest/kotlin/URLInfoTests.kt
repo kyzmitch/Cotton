deba28b71ecc0fbc2c85f24f3d712cba84a28a5c
Andrei Ermoshin <iermoshin@gmail.com>
2022-05-07 19:54:07 +0300

    Fix web search auto complete by adding separate parameter to URLInfo which is URL query which is not a part of URL path actually.

--- CoreHttpKit/src/commonTest/kotlin/URLInfoTests.kt
+++ CoreHttpKit/src/commonTest/kotlin/URLInfoTests.kt
@@ -6,16 +6,17 @@ import kotlin.test.assertEquals
 
 class URLInfoTests {
     val httpsScheme = HttpScheme.https
-    val urlLastPartGoogleDns = "resolve?name=apple.com&type=1&cd=false&do=false&edns_client_subnet=0.0.0.0%2F0"
-    val initialGoogleDnsUrlStr = "https://dns.google:443/" + urlLastPartGoogleDns
-    val withIpAddressGoogleDnsUrlStr = "https://127.0.0.1:443/" + urlLastPartGoogleDns
+    val urlPathGoogleDns = "resolve"
+    val urlQueryGoogleDns = "name=apple.com&type=1&cd=false&do=false&edns_client_subnet=0.0.0.0%2F0"
+    val initialGoogleDnsUrlStr = "https://dns.google:443/" + urlPathGoogleDns + "?" + urlQueryGoogleDns
+    val withIpAddressGoogleDnsUrlStr = "https://127.0.0.1:443/" + urlPathGoogleDns + "?" + urlQueryGoogleDns
 
     @Test
     fun testOriginalURL() {
         val googleDNStr = "dns.google"
         val googleDN = DomainName(googleDNStr)
         assertEquals(googleDNStr, googleDN.rawString)
-        val googleDsnIpInfo = URLInfo(httpsScheme, urlLastPartGoogleDns, googleDN)
+        val googleDsnIpInfo = URLInfo(httpsScheme, urlPathGoogleDns, urlQueryGoogleDns, googleDN)
         assertEquals(initialGoogleDnsUrlStr, googleDsnIpInfo.url)
         // should return the original URL if ip address is null
         assertEquals(initialGoogleDnsUrlStr, googleDsnIpInfo.urlWithIPaddress())
