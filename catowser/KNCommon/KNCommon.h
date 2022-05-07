a5934e2d347a1ef497f208ee20034a10488ed789
Andrei Ermoshin <iermoshin@gmail.com>
2022-05-07 11:36:55 +0300

    Fix Xcode archive operation by moving kotlin multiplatform CoreHttpKit SwiftPackage to a single Framework wrapper as it was done before for Alamfofire when it was moved to SPMCommon framework. Still there is an issue already on AppConnect side: ITMS-90562: Invalid Bundle - One or more dynamic libraries that are referenced by your app are not present in the dylib search path.

---
+++ catowser/KNCommon/KNCommon.h
@@ -0,0 +1,19 @@
+//
+//  KNCommon.h
+//  KNCommon
+//
+//  Created by Andrei Ermoshin on 5/7/22.
+//  Copyright © 2022 andreiermoshin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KNCommon.
+FOUNDATION_EXPORT double KNCommonVersionNumber;
+
+//! Project version string for KNCommon.
+FOUNDATION_EXPORT const unsigned char KNCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KNCommon/PublicHeader.h>
+
+
