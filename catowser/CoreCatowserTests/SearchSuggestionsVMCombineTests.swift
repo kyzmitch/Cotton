9f7420a94e00fb47291d692b6249e3d49a6940fe
Andrei Ermoshin <iermoshin@gmail.com>
2022-11-03 03:42:34 +0300

    One RX test for WebSearchAutocomplete

--- catowser/CoreCatowserTests/SearchSuggestionsVMCombineTests.swift
+++ catowser/CoreCatowserTests/SearchSuggestionsVMCombineTests.swift
@@ -8,15 +8,45 @@
 
 import XCTest
 @testable import CoreCatowser
+import CoreHttpKit
+import HttpKit
+import ReactiveHttpKit
+import ReactiveSwift
+import BrowserNetworking
+
+struct MockedGoodResponse: ResponseType {
+    static var successCodes: [Int] {
+        return [200]
+    }
+}
 
 final class SearchSuggestionsVMCombineTests: XCTestCase {
-    private let rxSubscriber: HttpKitRxSubscriber = .init()
-    private let subscriber: HttpKitSubscriber = .init()
+    let goodServerMock: MockedGoodDnsServer = .init()
+    let goodJsonEncodingMock: MockedGoodJSONEncoding = .init()
+    // swiftlint:disable:next force_unwrapping
+    lazy var reachabilityMock = NetworkReachabilityAdapterMock(server: goodServerMock)!
+    typealias Observer = Signal<MockedGoodResponse, HttpError>.Observer
+    typealias ObserverWrapper = RxObserverWrapper<MockedGoodResponse, MockedGoodDnsServer, Observer>
+    private let subscriber: Sub<MockedGoodResponse, MockedGoodDnsServer> = .init()
+    private let rxSubscriber: RxSubscriber<MockedGoodResponse, MockedGoodDnsServer, ObserverWrapper> = .init()
     
-    func testExample() throws {
-        let restClient: RestInterfaceMock = .init()
-        let contextMock: RestClientContextMock = .init(restClient, rxSubscriber, subscriber)
+    func testWebSearchAutocomplete() throws {
+        let restClient: RestInterfaceMock = .init(server: goodServerMock,
+                                                  jsonEncoder: goodJsonEncodingMock,
+                                                  reachability: reachabilityMock,
+                                                  httpTimeout: 10)
+        let contextMock = RestClientContextMock(restClient, rxSubscriber, subscriber)
         let strategyMock: SearchAutocompleteStrategyMock = .init(contextMock)
+        
+        let autoCompleteFacade: WebSearchAutocomplete = .init(strategyMock)
+        let producer = autoCompleteFacade.rxFetchSuggestions("how to use")
+        let expected = ["how to use Swift", "how to use Kotlin"]
+        let expectationRxSuggestionFail = XCTestExpectation(description: "Suggestions were not received")
+        producer.startWithResult { result in
+            expectationRxSuggestionFail.fulfill()
+            // swiftlint:disable:next force_try
+            let received = try! result.get()
+            XCTAssertEqual(received, expected)
+        }
     }
-
 }
