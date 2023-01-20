82145f29ad80d537d2f1f31d03d07d5b42f94781
Andrei Ermoshin <iermoshin@gmail.com>
2023-01-21 01:02:44 +0300

    Fix path

--- catowserAndroid/settings.gradle.kts
+++ catowserAndroid/settings.gradle.kts
@@ -15,6 +15,5 @@ dependencyResolutionManagement {
 rootProject.name = "Cotton"
 include("app")
 include("browser-content")
-// TODO: fix module path
-// include("CoreHttpKit")
-// project("CoreHttpKit").projectDir = File("./../CoreHttpKit")
+include(":CoreHttpKit")
+project(":CoreHttpKit").projectDir = File("../CoreHttpKit")
