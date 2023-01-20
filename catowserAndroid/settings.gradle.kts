99a1fbc2c60a6d1b9cd518d149e832ae8d10a524
Andrei Ermoshin <iermoshin@gmail.com>
2023-01-21 00:57:32 +0300

    Convert gradle files to kotlin format

--- catowserAndroid/settings.gradle.kts
+++ catowserAndroid/settings.gradle.kts
@@ -13,7 +13,8 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Cotton"
-include ':app'
-include ':browser-content'
-include ':CoreHttpKit'
-project(':CoreHttpKit').projectDir=new File('../CoreHttpKit')
+include("app")
+include("browser-content")
+// TODO: fix module path
+// include("CoreHttpKit")
+// project("CoreHttpKit").projectDir = File("./../CoreHttpKit")
