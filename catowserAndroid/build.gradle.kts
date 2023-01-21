bbd5ac701a5541334139be690349568f96136060
Andrei Ermoshin <iermoshin@gmail.com>
2023-01-21 11:19:54 +0300

    Attempt to fix gradle scripts

--- catowserAndroid/build.gradle.kts
+++ catowserAndroid/build.gradle.kts
@@ -7,5 +7,6 @@ buildscript {
 plugins {
     id("com.android.application") version "7.3.1" apply false
     id("com.android.library") version "7.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    kotlin("android") version "1.7.20" apply false
+    kotlin("multiplatform") version "1.7.20" apply false
 }
