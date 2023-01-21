5856b99baece2593768325d57732f4df08c03c44
Andrei Ermoshin <iermoshin@gmail.com>
2023-01-21 20:47:36 +0300

    Still gradle version error in Android project

--- catowserAndroid/build.gradle.kts
+++ catowserAndroid/build.gradle.kts
@@ -2,11 +2,14 @@ buildscript {
     extra.apply{
         set("compose_version", "1.1.1")
     }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.2")
+    }
 }// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "7.0.2" apply false
-    id("com.android.library") version "7.0.2" apply false
+    id("com.android.application") version "7.4"
+    id("com.android.library") version "7.4" apply false
     kotlin("android") version "1.8.0" apply false
     kotlin("multiplatform") version "1.8.0" apply false
 }
