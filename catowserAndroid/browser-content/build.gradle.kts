ce0a43bc58adbe40f79fcd3b8890c6f4495022de
Andrei Ermoshin <iermoshin@gmail.com>
2023-01-31 23:13:58 +0300

    Another fix for android module

--- catowserAndroid/browser-content/build.gradle.kts
+++ catowserAndroid/browser-content/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.compose.runtime:runtime:${rootProject.extra.get("compose_version") as String}")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
