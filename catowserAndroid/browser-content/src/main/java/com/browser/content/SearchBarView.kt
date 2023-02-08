81847997812ae96d8d53527b9e32e4236bcbffd4
Andrei Ermoshin <iermoshin@gmail.com>
2023-02-08 23:08:52 +0600

    Fixes for search bar visual appearance

--- catowserAndroid/browser-content/src/main/java/com/browser/content/SearchBarView.kt
+++ catowserAndroid/browser-content/src/main/java/com/browser/content/SearchBarView.kt
@@ -3,10 +3,7 @@ package com.browser.content
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -45,64 +42,59 @@ fun SearchBarView(
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusRequester = remember { FocusRequester() }
 
-    TopAppBar(
-        title = {
-             Spacer(modifier = Modifier.width(0.dp))
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 2.dp)
+            .onFocusChanged { focusState ->
+                showClearButton = (focusState.isFocused)
+            }
+            .focusRequester(focusRequester),
+        value = searchText,
+        onValueChange = onSearchTextChanged,
+        placeholder = {
+            Text(
+                text = placeholderText,
+                color = Purple700)
         },
-        navigationIcon = {
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Purple200,
+            unfocusedIndicatorColor = Purple200,
+            backgroundColor = Purple200,
+            cursorColor = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+        ),
+        leadingIcon = {
             Icon(
                 imageVector = Icons.Filled.Search,
                 modifier = Modifier,
                 contentDescription = stringResource(id = R.string.icn_search_magnifier_icon_description)
             )
-    }, actions = {
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 2.dp)
-                .onFocusChanged { focusState ->
-                    showClearButton = (focusState.isFocused)
+        },
+        trailingIcon = {
+            AnimatedVisibility(
+                visible = showClearButton,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                IconButton(onClick = {
+                    keyboardController?.hide()
+                    onClearClick()
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = stringResource(id = R.string.icn_search_clear_content_description)
+                    )
                 }
-                .focusRequester(focusRequester),
-            value = searchText,
-            onValueChange = onSearchTextChanged,
-            placeholder = {
-                Text(
-                    text = placeholderText,
-                    color = Purple700)
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                focusedIndicatorColor = Purple200,
-                unfocusedIndicatorColor = Purple200,
-                backgroundColor = Purple200,
-                cursorColor = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
-            ),
-            trailingIcon = {
-                AnimatedVisibility(
-                    visible = showClearButton,
-                    enter = fadeIn(),
-                    exit = fadeOut()
-                ) {
-                    IconButton(onClick = {
-                        keyboardController?.hide()
-                        onClearClick()
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.Close,
-                            contentDescription = stringResource(id = R.string.icn_search_clear_content_description)
-                        )
-                    }
 
-                }
-            },
-            maxLines = 1,
-            singleLine = true,
-            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(onDone = {
-                keyboardController?.hide()
-            }),
-        )
-    }) // `TopAppBar` from `androidx.compose.material`
+            }
+        },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            keyboardController?.hide()
+        }),
+    ) // OutlinedTextField
     LaunchedEffect(Unit) {
         // Don't request focus to not have keyboard visible at app start `focusRequester.requestFocus()`
     }
