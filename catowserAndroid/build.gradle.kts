c730d00a283ae9a7c46dd7d3c5c5589c3dd3252d
Andrei Ermoshin <iermoshin@gmail.com>
2023-02-11 12:36:40 +0600

    Fix compose kotlin version compile error in gradle

--- catowserAndroid/build.gradle.kts
+++ catowserAndroid/build.gradle.kts
@@ -6,13 +6,16 @@ buildscript {
     }
     extra.apply{
         set("compose_version", "1.3.3")
-        set("kotlinVersion", "1.7.20")
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.android.tools.build:gradle:7.3.0")
     }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+} // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    kotlin("android") version "1.7.20" apply false
+}
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
