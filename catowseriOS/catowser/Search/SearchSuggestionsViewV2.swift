372b039701ebcf5e01d936d2737fab9ac4af3193
Andrei Ermoshin <iermoshin@gmail.com>
2023-03-30 01:23:46 +0300

    Search suggestions view - beta 1

--- catowseriOS/catowser/Search/SearchSuggestionsViewV2.swift
+++ catowseriOS/catowser/Search/SearchSuggestionsViewV2.swift
@@ -7,19 +7,65 @@
 //
 
 import SwiftUI
+import CoreCatowser
 
 struct SearchSuggestionsViewV2: View {
     @Binding private var searchQuery: String
     private weak var delegate: SearchSuggestionsListDelegate?
+    @State private var suggestions: SearchSuggestionsViewState = .waitingForQuery
+    private let vm: SearchSuggestionsViewModel
     
     init(_ searchQuery: Binding<String>,
          _ delegate: SearchSuggestionsListDelegate?) {
         _searchQuery = searchQuery
         self.delegate = delegate
+        vm = ViewModelFactory.shared.searchSuggestionsViewModel()
     }
     
     var body: some View {
-        // TODO: add list
-        Text(verbatim: "Suggestions list")
+        constructView()
     }
+    
+    private func constructView() -> some View {
+        switch suggestions {
+        case .waitingForQuery:
+            return AnyView(ProgressView()
+                .task {
+                    suggestions = await vm.aaFetchSuggestions(searchQuery)
+                })
+        case .knownDomainsLoaded(let knownDomains):
+            return AnyView(List {
+                Section {
+                    ForEach(knownDomains) { SuggestionRow(value: $0)}
+                } header: {
+                    Text(verbatim: suggestions.sectionTitle(section: 0) ?? "Known domains")
+                }
+            })
+        case .everythingLoaded(let knownDomains, let querySuggestions):
+            return AnyView(List {
+                Section {
+                    ForEach(knownDomains) { SuggestionRow(value: $0)}
+                } header: {
+                    Text(verbatim: suggestions.sectionTitle(section: 0) ?? "Known domains")
+                }
+                Section {
+                    ForEach(querySuggestions) { SuggestionRow(value: $0)}
+                } header: {
+                    Text(verbatim: suggestions.sectionTitle(section: 1) ?? "Suggestions from search engine")
+                }
+            })
+        }
+    }
+}
+
+private struct SuggestionRow: View {
+    var value: String
+    
+    var body: some View {
+        Text(value)
+    }
+}
+
+private extension String {
+    static let globalSectionTtl = NSLocalizedString("ttl_global_menu", comment: "")
 }
