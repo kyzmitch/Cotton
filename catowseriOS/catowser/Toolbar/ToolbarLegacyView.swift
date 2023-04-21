69ae41d6d00d0746ef2e078118385b0323e0768f
Andrei Ermoshin <iermoshin@gmail.com>
2023-04-21 08:46:41 +0300

    Move toolbar view to a separate file

---
+++ catowseriOS/catowser/Toolbar/ToolbarLegacyView.swift
@@ -0,0 +1,40 @@
+//
+//  ToolbarLegacyView.swift
+//  catowser
+//
+//  Created by Andrei Ermoshin on 4/21/23.
+//  Copyright © 2023 andreiermoshin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ToolbarLegacyView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIViewController
+    @Binding private var webViewInterface: WebViewNavigatable?
+    
+    init(_ webViewInterface: Binding<WebViewNavigatable?>) {
+        _webViewInterface = webViewInterface
+    }
+    
+    private var vcFactory: ViewControllerFactory {
+        ViewsEnvironment.shared.vcFactory
+    }
+    
+    func makeUIViewController(context: Context) -> UIViewControllerType {
+        let interface = context.environment.browserContentCoordinators
+        let vc = vcFactory.toolbarViewController(nil,
+                                                 interface?.globalMenuDelegate,
+                                                 interface?.toolbarCoordinator,
+                                                 interface?.toolbarPresenter)
+        // swiftlint:disable:next force_unwrapping
+        return vc!
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        guard let vc = uiViewController as? WebBrowserToolbarController<MainToolbarCoordinator> else {
+            return
+        }
+        // This is the only way to set the web view interface for the toolbar
+        vc.siteNavigator = webViewInterface
+    }
+}
