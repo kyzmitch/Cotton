20475d8fce1950c2a8a79ee658f0a9e8191196ef
Andrei Ermoshin <iermoshin@gmail.com>
2023-05-02 20:18:05 +0300

    fix to site identifiable

--- catowseriOS/catowser/Extensions/Site+Extension.swift
+++ catowseriOS/catowser/Extensions/Site+Extension.swift
@@ -88,12 +88,13 @@ extension Site {
     }
 }
 
+/// Needed for a swiftUI list view to show a list of different sites.
+/// `The purpose of Identifiable is to distinguish the identity of an entity from the state of an entity.`
+/// https://github.com/apple/swift-evolution/blob/main/proposals/0261-identifiable.md#concrete-conformances
+/// So, this shouldn't be similar to Hashable impl and it doesn't require to combine all the properites in one id value.
 extension Site: Identifiable {
     public var id: String {
-        let settingsStr = "\(settings.isPrivate)\(settings.blockPopups)\(settings.isJSEnabled)\(settings.canLoadPlugins)"
-        let title = userSpecifiedTitle ?? ""
-        let urlStr = urlInfo.platformURL.absoluteString
-        let suggestion = searchSuggestion ?? ""
-        return title + urlStr + settingsStr + suggestion
+        // So, it is partly depencs on a state
+        urlInfo.platformURL.absoluteString
     }
 }
