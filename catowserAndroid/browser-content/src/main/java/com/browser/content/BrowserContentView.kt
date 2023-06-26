6a0c720027c985c25ca57542a29bfba429e2b1a2
Andrei Ermoshin <iermoshin@gmail.com>
2023-06-26 20:22:52 +0300

    Move TabContentType to a new file

--- catowserAndroid/browser-content/src/main/java/com/browser/content/BrowserContentView.kt
+++ catowserAndroid/browser-content/src/main/java/com/browser/content/BrowserContentView.kt
@@ -3,15 +3,7 @@ package com.browser.content
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import org.cotton.base.Site
-
-sealed class TabContentType {
-    class Blank(): TabContentType()
-    class TopSites(): TabContentType()
-    class Favorites(): TabContentType()
-    class Homepage(): TabContentType()
-    class SiteContent(val site: Site): TabContentType()
-}
+import com.browser.content.TabContentType
 
 @Composable
 fun BrowserContent(contentType: TabContentType) {
