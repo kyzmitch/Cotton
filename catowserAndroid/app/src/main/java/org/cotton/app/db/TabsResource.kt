5a02fec3aa99360e669bff64e038c7829cf017c2
Andrei Ermoshin <iermoshin@gmail.com>
2023-12-30 10:26:15 +0600

    Add implementation to TabsResource class

--- catowserAndroid/app/src/main/java/org/cotton/app/db/TabsResource.kt
+++ catowserAndroid/app/src/main/java/org/cotton/app/db/TabsResource.kt
@@ -1,6 +1,8 @@
 package org.cotton.app.db
 
 import android.content.Context
+import org.cotton.browser.content.data.Tab
+import java.util.UUID
 
 class TabsResource(private val context: Context) {
     private val tabsDbClient by lazy {
@@ -8,5 +10,51 @@ class TabsResource(private val context: Context) {
     }
     private val appSettingsDbClient by lazy {
         TabsDBClient.getDatabase(context).appSettingsDao()
+    }
+
+    /**
+     * Gets an identifier of a selected tab or an error if no tab is present
+     * which isn't possible at least blank tab should be present.
+     * */
+    fun selectedTabId(): UUID {
+        return appSettingsDbClient.fetchSelectedTabId()
+    }
+
+    fun remember(tab: Tab, select: Boolean) {
+        tabsDbClient.add(tab)
+        appSettingsDbClient.select(tab.id)
+    }
+
+    /**
+     * Gets all tabs recorded in DB. Currently there is only one session, but later
+     * it should be possible to store and read tabs from different sessions like
+     * private browser session tabs & usual tabs.
+     * */
+    fun tabsFromLastSession(): List<Tab> {
+        return tabsDbClient.fetchAllTabs()
+    }
+
+    /**
+     * Remembers tab identifier as selected one
+     * */
+    fun select(tab: Tab) {
+        appSettingsDbClient.select(tab.id)
+    }
+
+    /**
+     * Forgets specific tabs.
+     * If one of removed tabs was selected, then
+     * it is not really possible to handle that on this level,
+     * because if you deselect the tab, then need to select
+     * something new which is unknown and can be based on
+     * selection strategy on the upper level.
+     * */
+    suspend fun forget(tabs: List<Tab>) {
+        val identifiers = tabs.map {it.id }
+        tabsDbClient.remove(identifiers)
+    }
+
+    suspend fun update(tab: Tab) {
+        tabsDbClient.update(tab)
     }
 }
